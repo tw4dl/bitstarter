1c0882029f0b4b53aae5db385519badd753a80aa
David Liu <email4dliu@gmail.com>
2013-07-10 03:49:13 +0000

    use fs

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    fs.readFile('index.html', function (err, data) {
+	if (err) throw err;
+	response.send(data);
+	console.log(data);
+    });
+
 });
 
 var port = process.env.PORT || 5000;
